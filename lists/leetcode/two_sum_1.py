23b63c1ac574412c7e5a8939fe5db3b5a5a01e3d
Navin Kumar <naveen569652@gmail.com>
2025-06-01 11:06:44 +0530

    Two sum leetcode 1

---
+++ lists/leetcode/two_sum_1.py
@@ -0,0 +1,22 @@
+def twoSum(nums, target):
+    num_map = {}  # to store number and its index
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
