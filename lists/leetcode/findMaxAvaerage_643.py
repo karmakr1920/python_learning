2a5fe1f9871e9fee9bbc85628265761a8c473352
Navin Kumar <naveen569652@gmail.com>
2025-06-02 20:36:39 +0530

    643. Maximum Average Subarray I

---
+++ lists/leetcode/findMaxAvaerage_643.py
@@ -0,0 +1,19 @@
+def findMaxAverage(nums, k):
+    window_sum = sum(nums[:k])
+    max_sum = window_sum
+    
+    for i in range(k, len(nums)):
+        window_sum += nums[i] - nums[i - k]
+        max_sum = max(max_sum, window_sum)
+    
+    return max_sum / k
+
+# Example 1:
+
+# Input: nums = [1,12,-5,-6,50,3], k = 4
+# Output: 12.75000
+# Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+# Example 2:
+
+# Input: nums = [5], k = 1
+# Output: 5.00000
