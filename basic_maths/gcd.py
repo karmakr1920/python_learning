b1f4f9c4c8d2e912f2d6777300eb893ca3ba59e5
Navin Kumar <naveen569652@gmail.com>
2025-09-01 21:23:26 +0530

    GCD of two numbers

---
+++ basic_maths/gcd.py
@@ -0,0 +1,16 @@
+def GCD(self, n1, n2):
+    # Variable to store the gcd
+    gcd = 1
+    
+    # Iterate from 1 to min(n1, n2)
+    for i in range(1, min(n1, n2) + 1):
+        
+        # Check if i is a common
+        # divisor of both n1 and n2
+        if n1 % i == 0 and n2 % i == 0:
+            
+            # Update gcd
+            gcd = i
+    
+    # Return stored GCD.
+    return gcd
