dfe7b07d6cb5e5405a287f60744120e0a2af468a
Navin Kumar <naveen569652@gmail.com>
2025-09-02 20:09:32 +0530

    Check if the Array is Sorted I

---
+++ basic_arrays/arraySortedOrNot.py
@@ -0,0 +1,13 @@
+def arraySortedOrNot(self, arr, n):
+    # Iterate through the array
+    for i in range(n - 1):
+        
+        # Compare each element with the next one
+        if arr[i] > arr[i + 1]:
+            
+        # If any element is greater than the next one,
+        # the array is not sorted  
+            return False  
+            
+    # If no such pair is found, array is sorted
+    return True  
